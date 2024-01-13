7d89abc60fd44b5b233bacd9a3b1c99f43a4e3b8
staticWagomU <to6wa17@gmail.com>
2024-01-13 16:33:38 +0900

    init

---
+++ init.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+prompt $T$B$S$P$_$$$S
+doskey /macrofile=%USERPROFILE%\init.macros
+
+if "%CMD_INIT_SCRIPT_LOADED%" neq "" goto :eof
+set CMD_INIT_SCRIPT_LOADED=1
+set WEZTERM_CONFIG_FILE=%USERPROFILE%\wezterm.lua
+
+set LANG=ja_JP.UTF-8
+
+cls
---
+++ mklink.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal enabledelayedexpansion
+
+mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotfiles\config\nvim
+mklink /D C:\Users\wagomu\vimfiles C:\Users\wagomu\dotfiles\config\vim
