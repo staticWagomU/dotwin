6e30fc4b7bd57e491473604c22d5ceb9f918f8ff
staticWagomU <to6wa17@gmail.com>
2024-01-18 15:55:38 +0900

    update: init.cmdとinit.macrosのシンボリックリンクを作成するようにした

--- mklink.bat
+++ mklink.bat
@@ -3,3 +3,6 @@ setlocal enabledelayedexpansion
 
 mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotfiles\config\nvim
 mklink /D C:\Users\wagomu\vimfiles C:\Users\wagomu\dotfiles\config\vim
+
+mklink  %USERPROFILE%\init.cmd %USERPROFILE%\dotwin\init.cmd
+mklink  %USERPROFILE%\init.macros %USERPROFILE%\dotwin\init.macros
