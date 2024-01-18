7aaf273163af3b9015e8215b7908fca33d02876c
staticWagomU <to6wa17@gmail.com>
2024-01-18 15:57:14 +0900

    update: いらなくなった

--- mklink.bat
+++ mklink.bat
@@ -1,8 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 
-mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotfiles\config\nvim
-mklink /D C:\Users\wagomu\vimfiles C:\Users\wagomu\dotfiles\config\vim
-
 mklink  %USERPROFILE%\init.cmd %USERPROFILE%\dotwin\init.cmd
 mklink  %USERPROFILE%\init.macros %USERPROFILE%\dotwin\init.macros
