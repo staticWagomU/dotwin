a319ea255356ce26ced0025439f83aa928552d5a
staticWagomU <to6wa17@gmail.com>
2024-02-08 14:54:46 +0900

    bat: docker exec -it hoge bashをするコマンド。

---
+++ bat/pd.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+(for /f "skip=2 delims=" %%a in ('docker ps') do (
+    echo %%a
+)) > temp.txt
+
+type temp.txt | peco > selected.txt
+
+set /p selected=<selected.txt
+
+for /f "tokens=1" %%b in ("%selected%") do (
+    set CONTAINER_ID=%%b
+)
+
+echo Selected CONTAINER ID: %CONTAINER_ID%
+
+docker exec -it %CONTAINER_ID% bash
+
+del temp.txt
+del selected.txt
+
