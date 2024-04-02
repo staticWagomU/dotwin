424b3010850a67e47d0a42c733a1b57f5c668480
staticWagomU <to6wa17@gmail.com>
2024-04-03 08:30:24 +0900

    add aqua path

--- init.cmd
+++ init.cmd
@@ -2,11 +2,11 @@
 
 prompt $T$B$S$P$_$$$S
 doskey /macrofile=%USERPROFILE%\init.macros
-set PATH=C:\Users\wagomu\dotwin\bat;%PATH%
+set PATH=C:\Users\wagomu\dotwin\bat;%PATH%;%LOCALAPPDATA%\aquaproj-aqua\bat;%LOCALAPPDATA%\aquaproj-aqua\bin;%PATH%
 if "%CMD_INIT_SCRIPT_LOADED%" neq "" goto :eof
 set CMD_INIT_SCRIPT_LOADED=1
 set WEZTERM_CONFIG_FILE=%USERPROFILE%\wezterm.lua
-
+set AQUA_GLOBAL_CONFIG=%USERPROFILE%\aqua.yaml
 set LANG=ja_JP.UTF-8
 
 cls
