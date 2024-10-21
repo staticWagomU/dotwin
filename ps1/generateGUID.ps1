d4a8385f14b258a82083917e2b17beac9e286201
staticWagomU <to6wa17@gmail.com>
2024-10-21 11:01:03 +0900

    ps1: guidを生成するスクリプトよ

---
+++ ps1/generateGUID.ps1
@@ -0,0 +1,7 @@
+﻿$num = 64
+$outputFile = "$env:USERPROFILE/Desktop/guids.txt"
+
+# GUIDを生成してファイルに書き出す
+for ($i = 1; $i -le $num; $i++) {
+    [guid]::NewGuid().ToString().ToUpper() | Out-File -Append -FilePath $outputFile
+}
